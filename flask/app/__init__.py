72f034eed0dd285e6ea927a944d661b7f5b1fa7c
mohamad <mohamad@wazahub.com>
2020-02-04 11:33:54 +0100

    updates done

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,3 +1,3 @@
 from flask import Flask
 app = Flask(__name__)
-from app.views import mod as flask_welcome_mod
+from app.welcome.views import mod as flask_welcome_mod
--- flask/config.py
+++ flask/config.py
@@ -7,5 +7,5 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'transaction_service.db')
-SQLALCHEMY_DATABASE_URI = 'mysql://transactionservice:transactionservice@mysql_server/transactionservice'
+SQLALCHEMY_DATABASE_URI = 'mysql://flaskfirstprogram:flaskfirstprogram@flask_mysql_server/flaskfirstprogram'
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
